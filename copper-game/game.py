db7c6f05728549dd83b0d7c5c2c771471248dc0f
Kyle Johnsen <kaelstrom@gmail.com>
2012-09-29 23:31:15 -0400

    init

---
+++ copper-game/game.py
@@ -0,0 +1,25 @@
+'''
+Created on Sep 29, 2012
+
+@author: kaelstrom
+'''
+
+class Game(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def start(self):
+        print "Game start called"
+        pass
+    
+    
+if __name__ == '__main__':
+    pass
---
+++ copper-game/main.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 29, 2012
+
+@author: kaelstrom
+'''
+
+import game
+
+if __name__ == '__main__':
+    g = game.Game()
+    g.start()
