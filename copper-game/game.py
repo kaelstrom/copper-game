d42139fd72445277b7158b6be472dc326566c161
Kyle Johnsen <kaelstrom@gmail.com>
2012-10-20 21:54:20 -0400

    Revert "added contact info cards, expanded on swap game"
    This reverts commit 16b8502be956a34571f50430e7cb1d473bcf80ae.

--- copper-game/game.py
+++ copper-game/game.py
@@ -11,5 +11,5 @@ font_tempesta = None
 def init():
     global font_arial, font_tempesta
     pygame.init()
-    font_arial = pygame.font.SysFont("arial", 40)
+    font_arial = pygame.font.SysFont("arial", 48)
     font_tempesta = pygame.font.Font("tempesta.ttf", 8)
